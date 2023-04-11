89c0930f6d0627559089e66be945eda8f354c743
Samuel Carlisle <samuelcarlisle@gmail.com>
2023-04-11 18:43:07 +0200

    improved structure and experimenting with a js+css side

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+var myMenu; window.onload = function() {
+            myMenu = new SDMenu("my_menu");
+            myMenu.init();
+          };
