0090a4da79cac41187084c711df7d1f86b0db3b6
Samuel Carlisle <samuelcarlisle@gmail.com>
2023-04-14 19:24:09 +0200

    changed the menu

--- js/script.js
+++ js/script.js
@@ -4,7 +4,12 @@ if (document.getElementById('my-work-link')) {
   })
 }
 
-var myMenu; window.onload = function() {
-            myMenu = new SDMenu("my_menu");
-            myMenu.init();
-          };
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+} 
