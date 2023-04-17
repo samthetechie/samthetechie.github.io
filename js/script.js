a690c771dd3331262c32420b634133402375a178
Samuel Carlisle <samuelcarlisle@gmail.com>
2023-04-17 18:11:53 +0200

    some changes...

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 if (document.getElementById('my-work-link')) {
   document.getElementById('my-work-link').addEventListener('click', () => {
-    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
-  })
+    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"});
+  });
 }
 
 /* Open when someone clicks on the span element */
@@ -12,4 +12,12 @@ function openNav() {
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
-} 
+}
+
+document.querySelector("input, textarea").addEventListener("focusout", function(){
+    document.querySelector('meta[name=viewport]').remove();
+    document.head.insertAdjacentHTML('beforeend', '<meta name="viewport" content="width=device-width, maximum-scale=1.0, user-scalable=0">');
+
+    document.querySelector('meta[name=viewport]').remove();
+    document.head.insertAdjacentHTML('beforeend', '<meta name="viewport" content="width=device-width, initial-scale=yes">' );
+});
