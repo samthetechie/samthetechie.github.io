8a6b860614c4b9b242c3c66238bbdb6399e2c3ef
Samuel Carlisle <samuelcarlisle@gmail.com>
2023-04-18 19:45:13 +0200

    fixed refs in JS and DOM on index page for links to work

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-if (document.getElementById('my-work-link')) {
-  document.getElementById('my-work-link').addEventListener('click', () => {
-    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"});
+if (document.getElementById('my-service-link')) {
+  document.getElementById('my-service-link').addEventListener('click', () => {
+    document.getElementById('my-service-section').scrollIntoView({behavior: "smooth"});
   });
 }
 
 if (document.getElementById('my-project-link')) {
   document.getElementById('my-project-link').addEventListener('click', () => {
     document.getElementById('my-project-section').scrollIntoView({behavior: "smooth"});
+  });
+}
+
+if (document.getElementById('my-action-link')) {
+  document.getElementById('my-action-link').addEventListener('click', () => {
+    document.getElementById('my-action-section').scrollIntoView({behavior: "smooth"});
   });
 }
 
